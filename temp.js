e94714937a9d14e766ccea7a9f446343dc2303b8
NoopyCai <popparty3310@gmail.com>
2024-11-24 11:16:35 +0800

    js practice

---
+++ temp.js
@@ -0,0 +1,32 @@
+class Character{
+    constructor(name, weapon){
+        this.name = name;
+        this.weapon = weapon;
+    }
+    attack(){
+        return 'attack with ' + this.weapon;
+    }
+}
+
+class Elf extends Character{
+    constructor(name , weapon, type){
+        super(name, weapon);
+        this.type = type;
+    }
+}
+
+class Ogre extends Character{
+    constructor(name, weapon, color){
+        super(name, weapon);
+        this.color = color;
+    }
+    makeFort(){
+        return 'strongest fort in the world made';    
+    }
+}
+
+const dolby = new Elf('Dolby', 'cloth', 'house');
+const shrek = new Ogre('Shrek', 'club', 'green');
+console.log(Ogre.prototype.isPrototypeOf(shrek));
+console.log(Character.prototype.isPrototypeOf(Ogre.prototype));
+console.log(dolby instanceof Ogre);
