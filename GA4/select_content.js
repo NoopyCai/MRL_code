4678ecb10b059a7c8256665824a3c82573c7e664
NoopyCai <popparty3310@gmail.com>
2025-03-18 12:02:06 +0800

    feat:新增可能發生select_content的地方

--- GA4/select_content.js
+++ GA4/select_content.js
@@ -1,5 +1,5 @@
 function() {
     var clickElement = {{Click Element}};
-    var contentType = {{Click Element}}.getAttribute('ga-content-type') || clickElement.closest('[ga-content-type]').getAttribute('ga-content-type');
+    var contentType = {{Click Element}}.getAttribute('ga-content-type') || clickElement.closest('.select-content-parent').getAttribute('ga-content-type') || clickElement.closest('[ga-content-type]').getAttribute('ga-content-type');
     return contentType;
 }
